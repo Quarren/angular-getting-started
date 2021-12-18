5c4ce115cd87f448ac9be144d951e89cf5c81890
Quarren <pares.thibault@outlook.com>
2021-12-18 17:53:34 +0100

    shipping component added

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -25,6 +25,8 @@ export class CartService {
   }
 
   getShippingPrices() {
-    return this.http.get<{type: string, price: number}[]>('/assets/shipping.json');
+    return this.http.get<{ type: string; price: number }[]>(
+      '/assets/shipping.json'
+    );
   }
 }
